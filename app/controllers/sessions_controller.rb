013e2cd55a70c734f99a3d00677482140b50883c
Mary Taylor <marykate.taylor@gmail.com>
2020-05-04 16:17:20 -0400

    log in sign up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def destroy
+
+    reset_session
+
+    flash[:success] = "You are now logged out"
+
+    redirect_to root_path
+  end
 end
