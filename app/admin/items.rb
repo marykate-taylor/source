ab1cfbffed313b79f3ed3a67a53f2e7c9ded308f
Mary Taylor <marykate.taylor@gmail.com>
2020-05-12 16:54:10 -0400

    category and items connect

--- app/admin/items.rb
+++ app/admin/items.rb
@@ -1,16 +1,15 @@
 ActiveAdmin.register Item do
 
-  permit_params :title, :description, :image
+  permit_params :title, :description, :image, category_ids: []
 
-  form do |f|
-    f.inputs do
-      f.input :title
-      f.input :description
-      f.input :image
-      f.input :categories, as :check_boxes, collection: Category.order("title asc")
+    form do |f|
+      f.inputs do
+        f.input :title
+        f.input :description
+        f.input :image
+        f.input :categories, as: :check_boxes, collection: Category.order("title asc")
+      end
+
+      f.actions
     end
-
-    f.actions
-  end
-
 end
