c01bf66c9de8ba7abc9aa3ebf21554491b087267
Mary Taylor <marykate.taylor@gmail.com>
2020-05-12 16:13:12 -0400

    categories

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  resources :categories
+
   resources :items
 
   resources :users
